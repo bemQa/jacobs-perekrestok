150938e21009b8148d648f2ff799157505ca1ad1
Alexander K <niktonezapomnil@gmail.com>
2021-04-20 15:06:43 +0500

    parallax

--- js/scripts.js
+++ js/scripts.js
@@ -222,4 +222,22 @@ $(document).ready(function () {
             $(this).parent().removeClass('active');
         });
     }
+
+    function parallaxIt(e, target, movement, container) {
+        var $this = container;
+        var relX = e.pageX - $this.offset().left;
+        var relY = e.pageY - $this.offset().top;
+
+        TweenMax.to(target, 1, {
+            x: (relX - $this.width() / 2) / $this.width() * movement,
+            y: (relY - $this.height() / 2) / $this.height() * movement
+        });
+    }
+
+    if($(".main-page").length && window.innerWidth > 1000) {
+        $(".main-page").mousemove(function(e) {
+            var $this = $(this);
+            parallaxIt(e, ".main-decor", -15, $this);
+        });
+    }
 });
